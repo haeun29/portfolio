b9f2c00f955309a635a685fda503950cd09501bf
han <105402303+haeun29@users.noreply.github.com>
2022-08-25 16:58:33 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(function(){
+    $('#back-to-top').hide();
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 500){
+            $('#back-to-top').fadeIn();
+        }else { 
+            $('#back-to-top').fadeOut();
+        };
+    });
+
+
+    $('#back-to-top').click(function(){
+        $('html, body').animate({scrollTop : 0}, 400);
+        return false;
+    });
+
+    AOS.init();
+
+    const circle = document.querySelector("#circle");
+
+      document.addEventListener("mousemove", (e) => {
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+        circle.style.left = mouseX + "px";
+        circle.style.top = mouseY + "px";
+      });
+
+
+  
+
+})
+
